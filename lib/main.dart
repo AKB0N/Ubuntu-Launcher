8ea27a4b49bee38ee801efc6a86d5e363515296a
jspw <mhshifat757@gmail.com>
2022-02-14 00:38:42 +0600

    fix incompatibility with androidx1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:launcher/src/app.dart';
-import 'package:launcher/src/data/repositories/apps_repository.dart';
 
 void main() {
   runApp(MyApp());
